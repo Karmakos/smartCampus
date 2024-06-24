80c90fb3b5665c0330d96ef8fbddd07f802b6cf5
karmakos <karimakos.murigi@gmail.com>
2024-06-24 23:37:05 +0300

    Create basic files

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+//setting up the public folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const directory = path.join(__dirname, 'public');
+app.use(express.static(directory));
+
+//configuring the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Routes for Handlers
+app.get('/', (req, res) =>
+	{
+		res.render("index");
+		})
 
 
-
+app.listen(port, () =>
+	{
+		console.log(`App running at port ${port}`);
+	})
